848cabcdf5e02b20a084d6a6a5c8029db0ceba5e
Simon Koelsch <simon.koelsch@innoq.com>
2015-11-13 16:28:45 +0100

    pimped the run.sh script to use $1 as instance count and do the portmapping

--- run.sh
+++ run.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-for i in {1..50}; do  docker run -d redis ; done
+if [ -z ${1+x} ]; then 
+	echo "usage: ./run.sh INSTANCE_COUNT"
+else
+	for (( i=1; i<=$1; i++ )) do docker run -d --name="redis_$i" -p 637$i:6379 redis ; done
+fi;
+echo "portmapping is prefixed with 637<INSTANCE>, e.g. use 63742 to connect to instance 42"
